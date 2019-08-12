867cc5f10761285b44022eb2341b59da67a27749
Kevin Mathew <35139938+Kevin2498@users.noreply.github.com>
2019-08-13 01:32:43 +0530

    Add files via upload

---
+++ polyRegression.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 23:47:52 2019
+
+@author: HP
+"""
+#Polynomial Regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
